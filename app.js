d0a0ca3e7576baf31a98811f04cd8046b025048d
Anton Kulchytskyi <antek1809@gmail.com>
2022-09-29 06:05:21 +0200

    cors fixed

--- app.js
+++ app.js
@@ -14,21 +14,21 @@ const User = require("./db/userModel");
 // execute database connection 
 dbConnect();
 
-app.use(cors()); // one more solution for cors problem
+// app.use(cors()); // one more solution for cors problem
 
 // Curb Cores Error by adding a header here
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 // body parser configuration
 app.use(bodyParser.json());
